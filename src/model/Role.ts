38414e01581adfa3e560d80f3ede482583e13e56
czy12345 <1253665625@qq.com>
2018-02-12 16:28:12 +0800

    add annotation

--- src/model/Role.ts
+++ src/model/Role.ts
@@ -1,24 +1,30 @@
-import { Entity, Column, PrimaryGeneratedColumn, Index, ManyToOne , ManyToMany ,JoinTable, JoinColumn} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Index, ManyToOne, ManyToMany, JoinTable, JoinColumn } from 'typeorm';
 import { Func } from './Func';
 import { Module } from './Module';
 
+/* 角色，一般由后台创建，一个角色包含多个功能，进而包含了功能下的权限，具有指定特定操作的权限
+   角色不能跨模块，所以它只能包含属于同一个模块下的功能，当删除模块时，其所属角色也会删除
+*/
 @Entity('role')
-export class Role{
+@Index('name_module_id',['name','module_id'])
+export class Role {
 
     @PrimaryGeneratedColumn()
-    id:number;
+    id: number;
 
+    /* 角色名称，同一个模块下不能重复 */
     @Column({
-        name:'name',
-        type:'varchar',
-        length:20
+        name: 'name',
+        type: 'varchar',
+        length: 20
     })
-    name:string;
+    name: string;
 
-    @ManyToMany(type=>Func,{
+    /* 角色包含功能，只能包含同一个模块下功能 */
+    @ManyToMany(type => Func, {
         cascadeInsert: true,
         cascadeUpdate: false,
-        lazy:false
+        lazy: false
     })
     @JoinTable({
         name: 'role_function',
@@ -32,18 +38,23 @@ export class Role{
         },
         database: 'user_pm'
     })
-    funcs:Func[];
+    funcs: Func[];
 
-    @ManyToOne(type=>Module,module=>module.roles,{
-        cascadeInsert:true,
+    /* 所属模块id */
+    @Column()
+    module_id: number
+
+    /* 角色所属模块 */
+    @ManyToOne(type => Module, module => module.roles, {
+        cascadeInsert: true,
         cascadeUpdate: false,
         cascadeRemove: false,
         nullable: false,
-        lazy:false
+        lazy: false
     })
     @JoinColumn({
-        name:'module_id',
-        referencedColumnName:'id'
+        name: 'module_id',
+        referencedColumnName: 'id'
     })
-    module:Module;
+    module: Module;
 }
