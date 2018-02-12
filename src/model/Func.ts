dcaff9ff511dd371d3cf57a5f30f14bf2c65510e
czy12345 <1253665625@qq.com>
2018-02-12 15:58:51 +0800

    add annotation

--- src/model/Func.ts
+++ src/model/Func.ts
@@ -2,6 +2,10 @@ import { Entity, Column, PrimaryGeneratedColumn, Index, ManyToOne , ManyToMany ,
 import { Permission } from './Permission';
 import { Module } from './Module';
 
+/* 功能实体，功能是权限的集合
+   功能与权限是多对多关系，但是功能是属于某个模块的，一个功能所包含的权限也都是同一个模块的
+   模块删除时，其下包含的功能将会一起删除
+*/
 @Entity({
     name:'function'
 })
@@ -13,13 +17,16 @@ export class Func{
     })
     id:number;
 
+    /* 名称不能重复 */
     @Column({
         name:'name',
         type:'varchar',
-        length:20
+        length:20,
+        unique:true
     })
     name:string;
 
+    /* 功能下的所有权限，这些权限也都是属于一个模块的 */
     @ManyToMany(type=>Permission,{
         cascadeInsert: true,
         cascadeUpdate: false,
@@ -39,6 +46,7 @@ export class Func{
     })
     permissions:Permission[];
 
+    /* 功能所属模块  */
     @ManyToOne(type=>Module,module=>module.funcs,{
         cascadeInsert:true,
         cascadeUpdate: false,
@@ -51,5 +59,4 @@ export class Func{
         referencedColumnName:'id'
     })
     module:Module;
-
 }
