a4843faf2c2d3ea7a62d94436862401f85870cfc
czy12345 <1253665625@qq.com>
2018-02-12 16:23:39 +0800

    add annotation

--- src/model/Permission.ts
+++ src/model/Permission.ts
@@ -1,7 +1,12 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable ,Index, ManyToOne, JoinColumn} from 'typeorm';
 import { Module } from './Module';
 
-
+/* 权限实体，代表了执行某种操作的权限，属于某个模块
+   目前所有权限由模块中使用装饰器提供，找到权限之后保存它
+   不能在后台添加权限，因为添加也没用，权限的使用是写死在代码里的
+   同一个模块下权限名不能重复，不同模块下可以重复
+   当删除模块时，所有权限也会删除
+*/
 @Entity('permission')
 @Index('name_module_id',['name','module_id'])
 export class Permission{
@@ -9,6 +14,7 @@ export class Permission{
     @PrimaryGeneratedColumn()
     id:number;
 
+    /* 权限名，不同模块下可以重复 */
     @Column({
         name:'name',
         type:'varchar',
@@ -16,6 +22,7 @@ export class Permission{
     })
     name:string;
 
+    /* 权限描述 */
     @Column({
         name:'description',
         type:'varchar',
@@ -23,9 +30,11 @@ export class Permission{
     })
     description:string;
 
+    /* 所属模块id */
     @Column()
     module_id:number
 
+    /* 所属模块 */
     @ManyToOne(type=>Module,module=>module.permissions,{
         cascadeInsert: true,
         cascadeUpdate: false,
