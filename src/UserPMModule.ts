3fb417bf4084f9404e8551299ecb723768cae695
czy12345 <1253665625@qq.com>
2018-02-24 16:58:44 +0800

    add to Module

--- src/UserPMModule.ts
+++ src/UserPMModule.ts
@@ -2,6 +2,8 @@ import { OrganizationResolver } from './resolver/OrganizationResolver';
 import { RepositorysProvider } from './database/RepositorysProvider';
 import { OrganizationService } from './service/OrganizationService';
 import { ConnectionProvider } from './database/ConnectionProvider';
+import { InfoGroupResolver } from './resolver/InfoGroupResolver';
+import { InfoGroupService } from './service/InfoGroupService';
 import { UserResolver } from './resolver/UserResolver';
 import { UserService } from './service/UserService';
 import { Module, Global } from '@nestjs/common';
@@ -13,6 +15,7 @@ import { Module, Global } from '@nestjs/common';
   components: [
     ConnectionProvider, ...RepositorysProvider,
     OrganizationService, OrganizationResolver,
+    InfoGroupResolver, InfoGroupService,
     UserResolver, UserService
   ],
   exports: [...RepositorysProvider]
