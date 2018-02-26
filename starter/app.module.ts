db8d81a3207d239bf2813b018a73d4aee4c3067d
czy12345 <1253665625@qq.com>
2018-02-26 18:10:06 +0800

    correct

--- starter/app.module.ts
+++ starter/app.module.ts
@@ -19,12 +19,6 @@ export class ApplicationModule implements NestModule {
 
   constructor(
     @Inject(GraphQLFactory) private readonly graphQLFactory: GraphQLFactory,
-    @Inject('UserPMModule.FuncRepository') private readonly funcRepository: Repository<Func>,
-    @Inject('UserPMModule.RoleRepository') private readonly roleRepository: Repository<Role>,
-    @Inject('UserPMModule.UserRepository') private readonly userRepository: Repository<User>,
-    @Inject('UserPMModule.ModuleRepository') private readonly module1Repository: Repository<Module1>,
-    @Inject('UserPMModule.PermissionRepository') private readonly permissionRepository: Repository<Permission>,
-    @Inject('UserPMModule.OrganizationRepository') private readonly organizationRepository: Repository<Organization>
   ) { }
 
   configure(consumer: MiddlewaresConsumer) {
@@ -38,27 +32,5 @@ export class ApplicationModule implements NestModule {
   }
 
   async onModuleInit() {
-    /* 当OneToMany中cascadeInsert为true时，可以设置children，然后一次保存实体与父子关系，与ManyToOne中级联无关
-    let jituan  = this.organizationRepository.create({name:'集团'})
-    let renli  = this.organizationRepository.create({name:'人力'})
-    let bangongshi  = this.organizationRepository.create({name:'办公室'})
-    jituan.children = [renli,bangongshi]
-    await this.organizationRepository.save(jituan) 
-    */
-    /* 移除children只移除关联关系，而不删除children实体
-    let jituan  = this.organizationRepository.create({name:'集团'})
-    let renli  = this.organizationRepository.create({name:'人力'})
-    let bangongshi  = this.organizationRepository.create({name:'办公室'})
-    jituan.children = [renli,bangongshi]
-    await this.organizationRepository.save(jituan) 
-    jituan.children = []
-    await this.organizationRepository.save(jituan)  
-    */
-   /*  let jituan = this.organizationRepository.create({ name: '集团' })
-    let renli = this.organizationRepository.create({ name: '人力' })
-    let bangongshi = this.organizationRepository.create({ name: '办公室' })
-    jituan.children = [renli, bangongshi]
-    await this.organizationRepository.save(jituan)
-     */
   }
 }
