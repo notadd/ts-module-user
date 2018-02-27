b20a9b8a25bf7f0752a092e0ddd1c4b651ea2f68
czy12345 <1253665625@qq.com>
2018-02-27 19:01:24 +0800

    comment

--- src/subscriber/UserSubscriber.ts
+++ src/subscriber/UserSubscriber.ts
@@ -1,7 +1,10 @@
 import { EventSubscriber, EntitySubscriberInterface, InsertEvent, UpdateEvent, RemoveEvent,getRepository } from 'typeorm'  
 import { User } from '../model/User'  
   
-/* 用户的订阅器，用来在删除用户之前，解除用户与组织的关系 */
+/* 用户的订阅器，用来在删除用户之前，解除用户与组织的关系
+    之前的测试中发现删除组织时，组织、用户关系会自动解除，删除用户时会报错，所以创建了这个订阅器
+    后来又发现不会报错，删除用户可以直接删除，可能是使用了remove的缘故，暂时不管
+*/
 @EventSubscriber()  
 export class UserSubscriber implements EntitySubscriberInterface<User> {  
   
