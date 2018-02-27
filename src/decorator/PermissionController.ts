cc5844ea8d7f1fa98d75b872fcdf547e325101ca
czy12345 <1253665625@qq.com>
2018-02-27 15:22:40 +0800

    correct

--- src/decorator/PermissionController.ts
+++ src/decorator/PermissionController.ts
@@ -4,12 +4,12 @@ export const PERMISSION_CONTROLLER_AND = 'userpm:permission_controller_and'
 export function PermissionController(names: string[], mode: 'or' | 'and' = 'and') {
     return (target: object, key?, descriptor?) => {
         if (descriptor) {
-            let or_exist: string[] = Reflect.getMetadata(PERMISSION_CONTROLLER_OR, target, key) || []
-            let and_exist: string[] = Reflect.getMetadata(PERMISSION_CONTROLLER_AND, target, key) || []
+            let or_exist: string[] = Reflect.getMetadata(PERMISSION_CONTROLLER_OR, descriptor.value) || []
+            let and_exist: string[] = Reflect.getMetadata(PERMISSION_CONTROLLER_AND, descriptor.value) || []
             if (mode === 'or') {
-                Reflect.defineMetadata(PERMISSION_CONTROLLER_OR, names.concat(or_exist), target,key)
+                Reflect.defineMetadata(PERMISSION_CONTROLLER_OR, names.concat(or_exist), descriptor.value)
             } else if (mode === 'and') {
-                Reflect.defineMetadata(PERMISSION_CONTROLLER_AND, names.concat(and_exist), target,key)
+                Reflect.defineMetadata(PERMISSION_CONTROLLER_AND, names.concat(and_exist), descriptor.value)
             } else {
                 throw new Error('mode参数不正确')
             }
