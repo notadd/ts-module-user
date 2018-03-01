98b0eb24bfe747c7a23db47829d3ecaccd3704f1
czy12345 <1253665625@qq.com>
2018-03-01 14:33:26 +0800

    createScoreType service method

--- src/service/ScoreTypeService.ts
+++ src/service/ScoreTypeService.ts
@@ -14,4 +14,17 @@ export class ScoreTypeService {
         return await this.scoreTypeRepository.find()
     }
 
+    async createScoreType(name:string, type:string, description:string):Promise<void>{
+        let exist:ScoreType = await this.scoreTypeRepository.findOne({name})
+        if(exist){
+            throw new HttpException('指定名称积分类型已存在',424)
+        }
+        //方法中创建的积分项都是非默认的
+        let scoreType:ScoreType = this.scoreTypeRepository.create({name,type,default:false,description})
+        try {
+            await this.scoreTypeRepository.save(scoreType)
+        } catch (err) {
+            throw new HttpException('数据库错误' + err.toString(), 401)
+        }
+    }
 }
