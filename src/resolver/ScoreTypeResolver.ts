6a3d31b89d04a950a8d2518d3d8e34e85c5b0515
czy12345 <1253665625@qq.com>
2018-03-01 14:41:54 +0800

    updateScoreType

--- src/resolver/ScoreTypeResolver.ts
+++ src/resolver/ScoreTypeResolver.ts
@@ -62,4 +62,32 @@ export class ScoreTypeResolver {
         return data
     }
 
+    @Mutation('updateScoreType')
+    async updateScoreType(req: IncomingMessage, body: { id: number, name: string, type: string, description: string }): Promise<Data> {
+        let data: Data = {
+            code: 200,
+            message: '更新积分类型成功'
+        }
+        try {
+            let { id, name, type, description } = body
+            if (!id || !name || !type) {
+                throw new HttpException('缺少参数', 400)
+            }
+            if (type !== 'float' && type !== 'int') {
+                throw new HttpException('参数错误', 400)
+            }
+            await this.scoreTypeService.updateScoreType(id, name, type, description)
+        } catch (err) {
+            if (err instanceof HttpException) {
+                data.code = err.getStatus()
+                data.message = err.getResponse() + ''
+            } else {
+                console.log(err)
+                data.code = 500
+                data.message = '出现了意外错误' + err.toString()
+            }
+        }
+        return data
+    }
+
 }
--- src/service/ScoreTypeService.ts
+++ src/service/ScoreTypeService.ts
@@ -14,15 +14,39 @@ export class ScoreTypeService {
         return await this.scoreTypeRepository.find()
     }
 
-    async createScoreType(name:string, type:string, description:string):Promise<void>{
-        let exist:ScoreType = await this.scoreTypeRepository.findOne({name})
-        if(exist){
-            throw new HttpException('指定名称积分类型已存在',424)
+    async createScoreType(name: string, type: string, description: string): Promise<void> {
+        let exist: ScoreType = await this.scoreTypeRepository.findOne({ name })
+        if (exist) {
+            throw new HttpException('指定名称积分类型已存在', 424)
         }
         //方法中创建的积分项都是非默认的
-        let scoreType:ScoreType = this.scoreTypeRepository.create({name,type,default:false,description})
+        let scoreType: ScoreType = this.scoreTypeRepository.create({ name, type, default: false, description })
         try {
             await this.scoreTypeRepository.save(scoreType)
+        } catch (err) {
+            throw new HttpException('数据库错误' + err.toString(), 401)
+        }
+    }
+
+    async updateScoreType(id: number, name: string, type: string, description: string): Promise<void> {
+        let original: ScoreType = await this.scoreTypeRepository.findOneById(id)
+        if (!original) {
+            throw new HttpException('指定名id积分类型不存在', 425)
+        }
+        if (original.default) {
+            throw new HttpException('默认积分类型不允许更改', 425)
+        }
+        if(name!==original.name){
+            let exist:ScoreType = await this.scoreTypeRepository.findOne({name})
+            if(exist){
+                throw new HttpException('指定名称积分类型已存在', 424)
+            }
+        }
+        try {
+            original.name = name
+            original.type = type 
+            original.description = description
+            await this.scoreTypeRepository.save(original)
         } catch (err) {
             throw new HttpException('数据库错误' + err.toString(), 401)
         }
