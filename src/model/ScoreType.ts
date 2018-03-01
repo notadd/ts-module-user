2881eef819c8bfaae9e9ec40e1b01b7a1830ce93
czy12345 <1253665625@qq.com>
2018-03-01 11:52:49 +0800

    add ScoreType entity calss

---
+++ src/model/ScoreType.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+
+/* 积分类型 */
+@Entity('score_type')
+export class ScoreType {
+
+    @PrimaryGeneratedColumn({
+        name: 'id',
+        type: 'int'
+    })
+    id: number;
+
+    /* 积分类型名称，默认的积分类型有贡献、积分、余额、威望*/
+    @Column({
+        name: 'name',
+        type: 'varchar',
+        length: '20',
+        unique: true
+    })
+    name: string
+
+    /* 积分值类型，只能为float、int两种，根据类型不同，解析方式不同*/
+    @Column({
+        name: 'type',
+        type: 'enum',
+        enum: ['float', 'int']
+    })
+    type: string
+
+    /* 是否为默认积分类型，默认类型不可删除、更改 */
+    @Column({
+        name: 'default',
+        type: 'tinyint'
+    })
+    default: boolean
+
+    @Column({
+        name: 'description',
+        type: 'varchar'
+    })
+    description: string
+
+}
