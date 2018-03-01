0b695a56dbb99367cd4f880c2a1c7cc8f4ce13e9
czy12345 <1253665625@qq.com>
2018-03-01 14:10:30 +0800

    scoreTypes resolver method

--- src/resolver/ScoreTypeResolver.ts
+++ src/resolver/ScoreTypeResolver.ts
@@ -1,3 +1,4 @@
+import { ScoreTypesData } from '../interface/scoreType/ScoreTypesData';
 import { ScoreTypeService } from '../service/ScoreTypeService';
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { Inject, HttpException } from '@nestjs/common';
@@ -10,5 +11,27 @@ export class ScoreTypeResolver {
     constructor(
         @Inject(ScoreTypeService) private readonly scoreTypeService: ScoreTypeService
     ) { }
+
+    @Query('scoreTypes')
+    async scoreTypes(): Promise<ScoreTypesData> {
+        let data: ScoreTypesData = {
+            code: 200,
+            message: '获取所有积分类型成功',
+            scoreTypes: []
+        }
+        try {
+            data.scoreTypes = await this.scoreTypeService.getAll()
+        } catch (err) {
+            if (err instanceof HttpException) {
+                data.code = err.getStatus()
+                data.message = err.getResponse() + ''
+            } else {
+                console.log(err)
+                data.code = 500
+                data.message = '出现了意外错误' + err.toString()
+            }
+        }
+        return data
+    }
 
 }
