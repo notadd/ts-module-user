8f3156b88b467e8983de555b54a019021493991e
czy12345 <1253665625@qq.com>
2018-03-05 16:45:25 +0800

    add FuncService test 2

--- test/service/FuncServiceTest.spec.ts
+++ test/service/FuncServiceTest.spec.ts
@@ -5,26 +5,45 @@ import { TestingModule } from '@nestjs/testing/testing-module';
 import { FuncService } from '../../src/service/FuncService';
 import { HttpException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-
+import { Connection } from 'typeorm';
 describe('FuncService', async () => {
-    
-    let moduleBuilder: TestingModuleBuilder
 
+    let moduleBuilder: TestingModuleBuilder
+    let module: TestingModule
     describe('createFunc', async () => {
         beforeEach(async () => {
             moduleBuilder = await Test.createTestingModule({
-                components: [TestConnectionProvider, ...TestRepositorysProvider, FuncService]
+                components: [FuncService]
             })
         })
-        it('should throw HttpException: module no exist', async () => {
+        it('should throw HttpException: 指定模块token=aaaa不存在,415', async () => {
             let moduleRepository = { findOneById: async (moduleToken: string) => null }
-            let module: TestingModule = await moduleBuilder.overrideComponent('UserPMModule.ModuleRepository').useValue(moduleRepository).compile()
+            module = await moduleBuilder
+                .overrideComponent('UserPMModule.FuncRepository').useValue({})
+                .overrideComponent('UserPMModule.ModuleRepository').useValue(moduleRepository)
+                .overrideComponent('UserPMModule.PermissionRepository').useValue({})
+                .compile()
             let funcService: FuncService = module.get<FuncService>(FuncService)
-            try{
-                funcService.createFunc('aaaa','管理文章')
-            }catch(err){
+            try {
+                funcService.createFunc('aaaa', '管理文章')
+            } catch (err) {
                 expect(err.getStatus()).toBe(415)
                 expect(err.getResponse()).toBe('指定模块token=aaaa不存在')
+            }
+        })
+        it('should throw HttpException: 指定模块token=aaaa下，指定名称name=bbbb功能已经存在, 416', async () => {
+            let funcRepository = { findOne: async (func: any): Promise<any> => Promise.resolve({ id: 1, name: 'bbbb' }) }
+            module = await moduleBuilder
+                .overrideComponent('UserPMModule.FuncRepository').useValue(funcRepository)
+                .overrideComponent('UserPMModule.ModuleRepository').useValue({})
+                .overrideComponent('UserPMModule.PermissionRepository').useValue({})
+                .compile()
+            let funcService: FuncService = module.get<FuncService>(FuncService)
+            try {
+                funcService.createFunc('aaaa', 'bbbb')
+            } catch (err) {
+                expect(err.getStatus()).toBe(416)
+                expect(err.getResponse()).toBe('指定模块token=aaaa下，指定名称name=bbbb功能已经存在')
             }
         })
     })
