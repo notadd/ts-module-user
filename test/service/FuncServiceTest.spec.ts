d348c93cb0b67b5c73520c41c53b72619a1040fc
czy12345 <1253665625@qq.com>
2018-03-05 17:38:15 +0800

    add FuncService test 3

--- test/service/FuncServiceTest.spec.ts
+++ test/service/FuncServiceTest.spec.ts
@@ -8,7 +8,7 @@ describe('FuncService', async () => {
             let moduleRepository = { findOneById: async (moduleToken: string): Promise<any> => Promise.resolve(null) }
             let funcService: FuncService = new FuncService({} as any, moduleRepository as any, {} as any)
             try {
-                funcService.createFunc('aaaa', '管理文章')
+                await funcService.createFunc('aaaa', '管理文章')
             } catch (err) {
                 expect(err.getStatus()).toBe(415)
                 expect(err.getResponse()).toBe('指定模块token=aaaa不存在')
@@ -20,11 +20,27 @@ describe('FuncService', async () => {
             let moduleRepository = { findOneById: async (moduleToken: string): Promise<any> => Promise.resolve({ token: 'aaaa' }) }
             let funcService: FuncService = new FuncService(funcRepository as any, moduleRepository as any, {} as any)
             try {
-                funcService.createFunc('aaaa', 'bbbb')
+                await funcService.createFunc('aaaa', 'bbbb')
             } catch (err) {
                 expect(err.getStatus()).toBe(416)
                 expect(err.getResponse()).toBe('指定模块token=aaaa下，指定名称name=bbbb功能已经存在')
+            } 
+        })
+
+        it('should throw HttpException: 数据库错误Error: 保存功能失败, 401', async () => {
+            let funcRepository = {
+                create: (func: any): any => func,
+                findOne: async (func: any): Promise<any> => Promise.resolve(null),
+                save: async (func: any): Promise<void> => { throw new Error('保存功能失败') }
             }
+            let moduleRepository = { findOneById: async (moduleToken: string): Promise<any> => Promise.resolve({ token: 'aaaa' }) }
+            let funcService: FuncService = new FuncService(funcRepository as any, moduleRepository as any, {} as any)
+            try {
+                await funcService.createFunc('aaaa', 'bbbb')
+            } catch (err) {
+                expect(err.getStatus()).toBe(401)
+                expect(err.getResponse()).toBe('数据库错误Error: 保存功能失败')
+            } 
         })
     })
 })
