44f1753fb0a6b1958feb3122b7a59e5d5055985c
czy12345 <1253665625@qq.com>
2018-03-06 16:14:27 +0800

    createInfoItem test

--- test/service/InfoItemServiceTest.spec.ts
+++ test/service/InfoItemServiceTest.spec.ts
@@ -41,4 +41,33 @@ describe('FuncService', async () => {
         }
     })
 
+    describe('createInfoItem', async () => {
+
+        it('should success', async () => {
+            await infoItemService.createInfoItem('userName', '用户名', '用户的名称', 'text', true, 1)
+            let item = await infoItemRepository.findOne()
+            expect(item).toEqual({ id: 1, name: 'userName', label: '用户名', description: '用户的名称', type: 'text', necessary: 1, order: 1, default: 0 })
+        })
+
+        it('should throw HttpException:指定名称信息项已存在：userName, 412', async () => {
+            await infoItemRepository.save({ name: 'userName', label: '用户名', description: '用户的名称', type: 'text', necessary: true, order: 1, default: false })
+            try {
+                await infoItemService.createInfoItem('userName', '用户名', '用户的名称', 'text', true, 1)
+            } catch (err) {
+                expect(err.getStatus()).toBe(412)
+                expect(err.getResponse()).toBe('指定名称信息项已存在：userName')
+            }
+        })
+
+        it('should throw HttpException:数据库错误Error: 创建信息项失败，401', async () => {
+            jest.spyOn(infoItemRepository, 'save').mockImplementationOnce(async () => { throw new Error('创建信息项失败') })
+            try {
+                await infoItemService.createInfoItem('userName', '用户名', '用户的名称', 'text', true, 1)
+            } catch (err) {
+                expect(err.getStatus()).toBe(401)
+                expect(err.getResponse()).toBe('数据库错误Error: 创建信息项失败')
+            }
+        })
+    })
+
 })
