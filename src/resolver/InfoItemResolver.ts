7d00714b4cfa6eb99823a6908a22e84a08dad6d4
czy12345 <1253665625@qq.com>
2018-03-22 14:36:53 +0800

    ok

--- src/resolver/InfoItemResolver.ts
+++ src/resolver/InfoItemResolver.ts
@@ -1,12 +1,14 @@
+import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
+import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
 import { InfoItemService } from '../service/InfoItemService';
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
-import { Inject, HttpException } from '@nestjs/common';
 import { Data } from '../interface/Data';
 import { IncomingMessage } from 'http';
 
 
 /* 这个几个接口只是写在这，使用上还有很多问题,因为信息项可能不会编辑，所以不一定有用 */
 @Resolver('InfoItem')
+@UseInterceptors(ExceptionInterceptor)
 export class InfoItemResolver {
 
     constructor(
@@ -15,102 +17,42 @@ export class InfoItemResolver {
 
     @Mutation('createInfoItem')
     async createInfoItem(req: IncomingMessage, body: { name: string, label: string, description: string, type: string, necessary: boolean, registerVisible: boolean, informationVisible: boolean, order: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '创建信息项成功'
+        let { name, label, description, type, necessary, registerVisible, informationVisible, order } = body
+        if (!name || !label || !type || necessary === undefined || necessary === null) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { name, label, description, type, necessary, registerVisible, informationVisible, order } = body
-            if (!name || !label || !type || necessary === undefined || necessary === null) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.infoItemService.createInfoItem(name, label, description, type, necessary, registerVisible, informationVisible, order)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.infoItemService.createInfoItem(name, label, description, type, necessary, registerVisible, informationVisible, order)
+        return { code: 200, message: '创建信息项成功' }
     }
 
     @Mutation('updateInfoItem')
     async updateInfoItem(req: IncomingMessage, body: { id: number, name: string, label: string, description: string, type: string, necessary: boolean, registerVisible: boolean, informationVisible: boolean, order: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '更新信息项成功'
+        let { id, name, label, description, type, necessary, registerVisible, informationVisible, order } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { id, name, label, description, type, necessary, registerVisible, informationVisible, order } = body
-            if (!id) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.infoItemService.updateInfoItem(id, name, label, description, type, necessary, registerVisible, informationVisible, order)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.infoItemService.updateInfoItem(id, name, label, description, type, necessary, registerVisible, informationVisible, order)
+        return { code: 200, message: '更新信息项成功' }
     }
 
     @Mutation('deleteInfoItem')
     async deleteInfoItem(req: IncomingMessage, body: { id: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '删除信息项成功'
+        let { id } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { id } = body
-            if (!id) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.infoItemService.deleteInfoItem(id)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.infoItemService.deleteInfoItem(id)
+        return { code: 200, message: '删除信息项成功' }
     }
 
     @Mutation('deleteInfoItems')
     async deleteInfoItems(req: IncomingMessage, body: { ids: number[] }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '删除多个信息项成功'
+        let { ids } = body
+        if (!ids || ids.length === 0) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { ids } = body
-            if (!ids || ids.length === 0) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.infoItemService.deleteInfoItems(ids)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.infoItemService.deleteInfoItems(ids)
+        return { code: 200, message: '删除多个信息项成功' }
     }
 
 }
