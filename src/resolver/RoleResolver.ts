56ddf291adf8ea66c30be4078f9acaee4ebacba0
czy12345 <1253665625@qq.com>
2018-03-22 14:47:32 +0800

    ok

--- src/resolver/RoleResolver.ts
+++ src/resolver/RoleResolver.ts
@@ -1,14 +1,17 @@
+import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
+import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
-import { Inject, HttpException } from '@nestjs/common';
 import { RoleService } from '../service/RoleService';
+import { Func } from '../model/Func.entity';
+import { Role } from '../model/Role.entity';
 import { Data } from '../interface/Data';
 import { IncomingMessage } from 'http';
-import { Func } from '../model/Func';
-import { Role } from '../model/Role';
+
 
 
 /* 角色是功能的集合，因为功能属于模块，则角色也属于某个模块，随着模块删除而删除 */
 @Resolver('Role')
+@UseInterceptors(ExceptionInterceptor)
 export class RoleResolver {
 
     constructor(
@@ -17,101 +20,41 @@ export class RoleResolver {
 
     @Mutation('createRole')
     async createRole(req: IncomingMessage, body: { moduleToken: string, name: string, score: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '创建角色成功'
+        let { moduleToken, name, score } = body
+        if (!moduleToken || !name || !score) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { moduleToken, name, score } = body
-            if (!moduleToken || !name || !score) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.roleService.createRole(moduleToken, name, score)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.roleService.createRole(moduleToken, name, score)
+        return { code: 200, message: '创建角色成功' }
     }
 
     @Mutation('updateRole')
     async updateRole(req: IncomingMessage, body: { id: number, name: string, score: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '更新角色成功'
+        let { id, name, score } = body
+        if (!id || !name || !score) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { id, name, score } = body
-            if (!id || !name || !score) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.roleService.updateRole(id, name, score)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.roleService.updateRole(id, name, score)
+        return { code: 200, message: '更新角色成功' }
     }
 
     @Mutation('deleteRole')
     async deleteRole(req: IncomingMessage, body: { id: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '删除角色成功'
+        let { id } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { id } = body
-            if (!id) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.roleService.deleteRole(id)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.roleService.deleteRole(id)
+        return { code: 200, message: '删除角色成功' }
     }
 
     @Mutation('setFuncs')
     async setFuncs(req: IncomingMessage, body: { id: number, funcIds: number[] }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '设置角色功能成功'
+        let { id, funcIds } = body
+        if (!id) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { id, funcIds } = body
-            if (!id) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.roleService.setFuncs(id, funcIds)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.roleService.setFuncs(id, funcIds)
+        return { code: 200, message: '设置角色功能成功' }
     }
 }
--- src/resolver/ScoreResolver.ts
+++ src/resolver/ScoreResolver.ts
@@ -1,10 +1,12 @@
+import { ExceptionInterceptor } from '../interceptor/ExceptionInterceptor';
+import { Inject, HttpException, UseInterceptors } from '@nestjs/common';
 import { Resolver, Query, Mutation } from '@nestjs/graphql';
 import { ScoreService } from '../service/ScoreService';
-import { Inject, HttpException } from '@nestjs/common';
 import { Data } from '../interface/Data';
 import { IncomingMessage } from 'http';
 
 @Resolver('Score')
+@UseInterceptors(ExceptionInterceptor)
 export class ScoreResolver {
 
     constructor(
@@ -13,53 +15,22 @@ export class ScoreResolver {
 
     @Query('getScore')
     async getScore(req: IncomingMessage, body: { userId: number, scoreTypeId: number }): Promise<Data & { score: number }> {
-        let data: Data & { score: number } = {
-            code: 200,
-            message: '获取积分成功',
-            score: null
+        let { userId, scoreTypeId } = body
+        if (!userId || !scoreTypeId) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { userId, scoreTypeId } = body
-            if (!userId || !scoreTypeId) {
-                throw new HttpException('缺少参数', 400)
-            }
-            data.score = await this.scoreService.getScore(userId, scoreTypeId)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        let score = await this.scoreService.getScore(userId, scoreTypeId)
+        return { code: 200, message: '获取积分成功', score }
     }
 
     @Mutation('setScore')
     async setScore(req: IncomingMessage, body: { userId: number, scoreTypeId: number, add: number }): Promise<Data> {
-        let data: Data = {
-            code: 200,
-            message: '设置积分成功'
+        let { userId, scoreTypeId, add } = body
+        if (!userId || !scoreTypeId) {
+            throw new HttpException('缺少参数', 400)
         }
-        try {
-            let { userId, scoreTypeId, add } = body
-            if (!userId || !scoreTypeId) {
-                throw new HttpException('缺少参数', 400)
-            }
-            await this.scoreService.setScore(userId, scoreTypeId, add)
-        } catch (err) {
-            if (err instanceof HttpException) {
-                data.code = err.getStatus()
-                data.message = err.getResponse() + ''
-            } else {
-                console.log(err)
-                data.code = 500
-                data.message = '出现了意外错误' + err.toString()
-            }
-        }
-        return data
+        await this.scoreService.setScore(userId, scoreTypeId, add)
+        return { code: 200, message: '设置积分成功' }
     }
 
 
