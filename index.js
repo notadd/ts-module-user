2fc09e2737586c47b89edd6b447a1b0f33311262
czy12345 <1253665625@qq.com>
2018-05-07 09:37:59 +0800

    modify export

---
+++ index.js
@@ -0,0 +1,5 @@
+export * from "./src/user.module";
+export * from "./src/decorator/can.decorator";
+export * from "./src/decorator/permissions.decorator";
+export * from "./src/decorator/user.info.manager.decorator";
+export { UserComponent, UserComponentToken } from "./src/export/user.component.provider"
--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./src/user.module";
-export * from "./src/decorator/can.decorator";
-export * from "./src/decorator/permissions.decorator";
-export * from "./src/decorator/user.info.manager.decorator";
