93d4b5e06eebe75e0cd1de68a2e9b821db8c0aa7
czy12345 <1253665625@qq.com>
2018-05-07 10:36:33 +0800

    add UserInfoManager interface

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export * from "./src/decorator/can.decorator";
 export * from "./src/decorator/permissions.decorator";
 export * from "./src/decorator/user.info.manager.decorator";
 export { UserComponent, UserComponentToken } from "./src/export/user.component.provider"
+export * from "./src/interface/user.info.manager";
