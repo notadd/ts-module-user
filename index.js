6070b2b05d389a3aec08d7c83b8df4ff7a0220bc
czy12345 <1253665625@qq.com>
2018-05-08 14:13:54 +0800

    export User entity class

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-export * from "./src/user.module";
-export * from "./src/decorator/can.decorator";
-export * from "./src/decorator/permissions.decorator";
-export * from "./src/decorator/user.info.manager.decorator";
+export * from "./package/user.module";
+export * from "./package/decorator/can.decorator";
+export * from "./package/decorator/permissions.decorator";
 export { UserComponent, UserComponentToken } from "./src/export/user.component.provider"
-export * from "./src/interface/user.info.manager";
+export { User } from "./package/model/user.entity"
