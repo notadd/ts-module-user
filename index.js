f3e84135c43d8263faadc69919821fc09d388127
czy12345 <1253665625@qq.com>
2018-05-08 14:36:18 +0800

    add index.ts

--- index.js
+++
@@ -1,5 +0,0 @@
-export * from "./package/user.module";
-export * from "./package/decorator/can.decorator";
-export * from "./package/decorator/permissions.decorator";
-export { UserComponent, UserComponentToken } from "./src/export/user.component.provider"
-export { User } from "./package/model/user.entity"
--- package/index.js
+++ package/index.js
@@ -1,6 +1,15 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var user_module_1 = require("./user.module");
-exports.UserModule = user_module_1.UserModule;
+__export(require("./user.module"));
+__export(require("./decorator/can.decorator"));
+__export(require("./decorator/permissions.decorator"));
+var user_component_provider_1 = require("./export/user.component.provider");
+exports.UserComponent = user_component_provider_1.UserComponent;
+exports.UserComponentToken = user_component_provider_1.UserComponentToken;
+var user_entity_1 = require("./model/user.entity");
+exports.User = user_entity_1.User;
 
 //# sourceMappingURL=index.js.map
