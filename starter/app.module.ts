fd8eef3c95430ca22fba0e06e21adf86a4e9eed3
czy12345 <1253665625@qq.com>
2018-05-14 17:05:35 +0800

    update version success

--- starter/app.module.ts
+++ starter/app.module.ts
@@ -1,22 +1,20 @@
 import { Module, MiddlewareConsumer, NestModule, RequestMethod, Inject } from "@nestjs/common";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import { GraphQLModule, GraphQLFactory } from "@nestjs/graphql";
-import { UpyunModule } from "@notadd/addon-upyun";
+import { LocalModule } from "@notadd/addon-local";
 import { Connection, Repository } from "typeorm";
 import { UserModule } from "../src/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-
 @Module({
-  imports: [GraphQLModule, UpyunModule, TypeOrmModule.forRoot({
-    name: "user_pm",
+  imports: [GraphQLModule, LocalModule, TypeOrmModule.forRoot({
     type: "postgres",
     host: "localhost",
     port: 5433,
     username: "postgres",
     password: "123456",
     database: "postgres",
-    entities: ["../**/*.entity.ts"],
+    entities: ["./**/*.entity.ts", "./**/*.entity.js"],
     logger: "simple-console",
     synchronize: true,
     dropSchema: false
@@ -29,7 +27,7 @@ export class ApplicationModule implements NestModule {
   ) { }
 
   configure(consumer: MiddlewareConsumer) {
-    const typeDefs = this.graphQLFactory.mergeTypesByPaths("./src/**/*.types.graphql");
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths("./**/*.types.graphql");
     const schema = this.graphQLFactory.createSchema({ typeDefs });
     consumer
       .apply(graphiqlExpress({ endpointURL: "/graphql" }))
@@ -37,6 +35,5 @@ export class ApplicationModule implements NestModule {
       .apply(graphqlExpress(req => ({ schema, rootValue: req })))
       .forRoutes("/graphql");
   }
-
 
 }
