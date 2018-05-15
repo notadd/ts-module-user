d7b6bc1b15ad049361b4286bfe0eabea746b447a
czy12345 <1253665625@qq.com>
2018-05-15 11:29:39 +0800

    complete JwtStrategy

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -1,23 +1,39 @@
+import { Injectable, UnauthorizedException, Inject } from "@nestjs/common";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { AuthService } from "./auth.service";
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { JwtPayload } from "./interfaces/jwt-payload.interface";
+import { AuthService } from "./auth.service";
+import { User } from "../model/user.entity";
 
+/*
+认证策略，这里使用的是jsonwebtoken
+JwtStrategy继承了PassportStrategy类，这个类构造函数中，保证passport采用这个策略
+ */
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+
+  constructor(@Inject(AuthService) private readonly authService: AuthService) {
+    /* 调用父类构造函数，使passport采用本策略 */
     super({
+      /* token的获取方式，这里指的是从authorization请求头中获取`bearer ${token}`形式的token */
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: "secretKey",
+      secretOrKey: authService.secretKey,
+      // issuer?: string;
+      // audience?: string;
+      // algorithms?: string[];
+      ignoreExpiration: false,
+      passReqToCallback: false,
     });
   }
 
-  async validate(payload: JwtPayload, done: Function) {
+  /* passport直接使用的是这个验证函数，这个验证函数里面调用了AuthService里的验证函数 */
+  async validate(payload: User, done: Function) {
+    /* 调用AuthService里的验证函数 */
     const user = await this.authService.validateUser(payload);
+    /* 如果不存在，即返回false，则将异常传递给回调 */
     if (!user) {
       return done(new UnauthorizedException(), false);
     }
+    /* 如果存在将认证后的user返回给回调 */
     done(null, user);
   }
 }
