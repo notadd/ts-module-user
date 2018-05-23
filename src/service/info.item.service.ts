09afd09dfe74fc002529b648b01733e4891af928
czy12345 <1253665625@qq.com>
2018-05-23 11:17:20 +0800

    use template string

--- src/service/info.item.service.ts
+++ src/service/info.item.service.ts
@@ -15,10 +15,10 @@ export class InfoItemService {
     async createInfoItem(name: string, label: string, description: string, type: string, necessary: boolean, registerVisible: boolean, informationVisible: boolean, order: number): Promise<void> {
         const exist: InfoItem|undefined = await this.infoItemRepository.findOne({ name });
         if (exist) {
-            throw new HttpException("指定名称信息项已存在：" + name, 412);
+            throw new HttpException(`指定名称信息项已存在：${name}`, 412);
         }
         if (necessary && !registerVisible) {
-            throw new HttpException("指定名称name=" + name + "必填信息项，注册时必须可见", 412);
+            throw new HttpException(`指定名称name=${name}必填信息项，注册时必须可见`, 412);
         }
         const item: InfoItem = this.infoItemRepository.create({
             name,
@@ -34,7 +34,7 @@ export class InfoItemService {
         try {
             await this.infoItemRepository.save(item);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
@@ -42,19 +42,19 @@ export class InfoItemService {
     async updateInfoItem(id: number, name: string, label: string, description: string, type: string, necessary: boolean, registerVisible: boolean, informationVisible: boolean, order: number): Promise<void> {
         const exist: InfoItem|undefined = await this.infoItemRepository.findOne(id);
         if (!exist) {
-            throw new HttpException("指定id=" + id + "信息项不存在", 413);
+            throw new HttpException(`指定id=${id}信息项不存在`, 413);
         }
         // 默认信息项无法更新
         if (exist.default) {
             throw new HttpException("默认信息项不允许更新", 413);
         }
         if (necessary && !registerVisible) {
-            throw new HttpException("指定名称name=" + name + "必填信息项，注册时必须可见", 412);
+            throw new HttpException(`指定名称name=${name}必填信息项，注册时必须可见`, 412);
         }
         if (name !== exist.name) {
             const exist1: InfoItem|undefined = await this.infoItemRepository.findOne({ name });
             if (exist1) {
-                throw new HttpException("指定name=" + name + "信息项已存在", 412);
+                throw new HttpException(`指定name=${name}信息项已存在`, 412);
             }
         }
         exist.name = name;
@@ -68,7 +68,7 @@ export class InfoItemService {
         try {
             await this.infoItemRepository.save(exist);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
@@ -76,7 +76,7 @@ export class InfoItemService {
     async deleteInfoItem(id: number): Promise<void> {
         const exist: InfoItem|undefined = await this.infoItemRepository.findOne(id);
         if (!exist) {
-            throw new HttpException("指定id=" + id + "信息项不存在", 413);
+            throw new HttpException(`指定id=${id}信息项不存在`, 413);
         }
         // 默认信息项无法删除
         if (exist.default) {
@@ -85,7 +85,7 @@ export class InfoItemService {
         try {
             await this.infoItemRepository.remove(exist);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
@@ -98,7 +98,7 @@ export class InfoItemService {
                 return item.id === id;
             });
             if (!find) {
-                throw new HttpException("指定id=" + id + "信息项不存在", 413);
+                throw new HttpException(`指定id=${id}信息项不存在`, 413);
             }
             // 默认信息项无法删除
             if (find.default) {
@@ -108,7 +108,7 @@ export class InfoItemService {
         try {
             await this.infoItemRepository.remove(infoItems);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
