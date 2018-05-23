118762f26d57c55e798ef3abd427443bbb63aa75
czy12345 <1253665625@qq.com>
2018-05-23 11:24:21 +0800

    use template string

--- src/service/organization.service.ts
+++ src/service/organization.service.ts
@@ -21,7 +21,7 @@ export class OrganizationService {
     async getChildren(id: number): Promise<Array<Organization>> {
         const o: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "children" ] });
         if (!o) {
-            throw new HttpException("指定父组织id=" + id + "不存在", 402);
+            throw new HttpException(`指定父组织id=${id}不存在`, 402);
         }
         return o.children;
     }
@@ -35,38 +35,38 @@ export class OrganizationService {
         if (parentId !== undefined && parentId !== null) {
             parent = await this.organizationRepository.findOne(parentId);
             if (!parent) {
-                throw new HttpException("指定父组织id=" + parentId + "不存在", 402);
+                throw new HttpException(`指定父组织id=${parentId}不存在`, 402);
             }
         }
         const exist: Organization|undefined = await this.organizationRepository.findOne({ name });
         if (exist) {
-            throw new HttpException("指定名称name=" + name + "组织已存在", 403);
+            throw new HttpException(`指定名称name=${name}组织已存在`, 403);
         }
         // 如果parent为undefined，则parentId为null
         const organization: Organization = this.organizationRepository.create({ name, parent });
         try {
             await this.organizationRepository.save(organization);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async updateOrganization(id: number, name: string, parentId: number): Promise<void> {
         const exist: Organization|undefined = await this.organizationRepository.findOne(id);
         if (!exist) {
-            throw new HttpException("指定id=" + id + "组织不存在", 404);
+            throw new HttpException(`指定id=${id}组织不存在`, 404);
         }
         if (name !== exist.name) {
             const exist: Organization|undefined = await this.organizationRepository.findOne({ name });
             if (exist) {
-                throw new HttpException("指定name=" + name + "组织已存在", 404);
+                throw new HttpException(`指定name=${name}组织已存在`, 404);
             }
         }
         let parent: Organization|undefined ;
         if (parentId !== undefined && parentId !== null) {
             parent = await this.organizationRepository.findOne(parentId);
             if (!parent) {
-                throw new HttpException("指定父组织id=" + parentId + "不存在", 402);
+                throw new HttpException(`指定父组织id=${parentId}不存在`, 402);
             }
         }
         try {
@@ -76,14 +76,14 @@ export class OrganizationService {
             exist.parent = parent as any;
             await this.organizationRepository.save(exist);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async deleteOrganization(id: number): Promise<void> {
         const exist: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "children" ] });
         if (!exist) {
-            throw new HttpException("指定id=" + id + "组织不存在", 404);
+            throw new HttpException(`指定id=${id}组织不存在`, 404);
         }
         if (exist.children && exist.children.length > 0) {
             throw new HttpException("指定组织存在子组织，无法删除", 404);
@@ -91,14 +91,14 @@ export class OrganizationService {
         try {
             await this.organizationRepository.remove(exist);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async getUsersInOrganization(id: number): Promise<Array<User>> {
         const o: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "users" ] });
         if (!o) {
-            throw new HttpException("指定id=" + id + "父组织不存在", 402);
+            throw new HttpException(`指定id=${id}父组织不存在`, 402);
         }
         // 只获取不再回收站中的用户
         return o.users.filter(user => {
@@ -109,30 +109,30 @@ export class OrganizationService {
     async addUserToOrganization(id: number, userId: number): Promise<void> {
         const o: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "users" ] });
         if (!o) {
-            throw new HttpException("指定id=" + id + "组织不存在", 402);
+            throw new HttpException(`指定id=${id}组织不存在`, 402);
         }
         const user: User|undefined = await this.userRepository.findOne(userId);
         if (!user) {
-            throw new HttpException("指定id=" + userId + "用户不存在", 402);
+            throw new HttpException(`指定id=${userId}用户不存在`, 402);
         }
         const exist: User|undefined = o.users.find(user => {
             return user.id === userId;
         });
         if (exist) {
-            throw new HttpException("指定用户id=" + userId + "已存在于指定组织id=" + id + "中", 402);
+            throw new HttpException(`指定用户id=${userId}已存在于指定组织id=${id}中`, 402);
         }
         o.users.push(user);
         try {
             await this.organizationRepository.save(o);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async addUsersToOrganization(id: number, userIds: Array<number>): Promise<void> {
         const o: Organization |undefined = await this.organizationRepository.findOne(id, { relations: [ "users" ] });
         if (!o) {
-            throw new HttpException("指定id=" + id + "组织不存在", 402);
+            throw new HttpException(`指定id=${id}组织不存在`, 402);
         }
         const users: Array<User> = await this.userRepository.findByIds(userIds);
         // 验证是否所有需要的用户都被查询出来
@@ -141,7 +141,7 @@ export class OrganizationService {
                 return user.id === id;
             });
             if (!find) {
-                throw new HttpException("指定id=" + id + "用户不存在", 402);
+                throw new HttpException(`指定id=${id}用户不存在`, 402);
             }
         });
         // 验证是否有用户已存在于指定组织下
@@ -150,44 +150,44 @@ export class OrganizationService {
                 return id === user.id;
             });
             if (match) {
-                throw new HttpException("指定用户id=" + user.id + "已存在于指定组织id=" + id + "中", 402);
+                throw new HttpException(`指定用户id=${user.id}已存在于指定组织id=${id}中`, 402);
             }
         });
         o.users.push(...users);
         try {
             await this.organizationRepository.save(o);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async removeUserFromOrganization(id: number, userId: number): Promise<void> {
         const o: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "users" ] });
         if (!o) {
-            throw new HttpException("指定id=" + id + "组织不存在", 402);
+            throw new HttpException(`指定id=${id}组织不存在`, 402);
         }
         const user: User|undefined = await this.userRepository.findOne(userId);
         if (!user) {
-            throw new HttpException("指定id=" + userId + "用户不存在", 402);
+            throw new HttpException(`指定id=${userId}用户不存在`, 402);
         }
         const index = o.users.findIndex(user => {
             return user.id === userId;
         });
         if (index < 0) {
-            throw new HttpException("指定用户id=" + userId + "不存在于指定组织id=" + id + "中", 402);
+            throw new HttpException(`指定用户id=${userId}不存在于指定组织id=${id}中`, 402);
         }
         o.users.splice(index, 1);
         try {
             await this.organizationRepository.save(o);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async removeUsersFromOrganization(id: number, userIds: Array<number>): Promise<void> {
         const o: Organization|undefined = await this.organizationRepository.findOne(id, { relations: [ "users" ] });
         if (!o) {
-            throw new HttpException("指定id=" + id + "组织不存在", 402);
+            throw new HttpException(`指定id=${id}组织不存在`, 402);
         }
         const users: Array<User> = await this.userRepository.findByIds(userIds);
         // 从组织的用户中循环移除指定用户，要求用户存在于数据库中，且用户必须已经存在于指定组织中
@@ -196,20 +196,20 @@ export class OrganizationService {
                 return user.id === userId;
             });
             if (!find) {
-                throw new HttpException("指定id=" + userId + "用户不存在", 402);
+                throw new HttpException(`指定id=${userId}用户不存在`, 402);
             }
             const index = o.users.findIndex(user => {
                 return user.id === userId;
             });
             if (index < 0) {
-                throw new HttpException("指定用户id=" + userId + "不存在于指定组织id=" + id + "中", 402);
+                throw new HttpException(`指定用户id=${userId}不存在于指定组织id=${id}中`, 402);
             }
             o.users.splice(index, 1);
         });
         try {
             await this.organizationRepository.save(o);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 }
