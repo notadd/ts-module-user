57a38d50b1a1e8191287b49f53bf2c6273e4a967
czy12345 <1253665625@qq.com>
2018-05-23 11:37:08 +0800

    use template string

--- src/service/score.type.service.ts
+++ src/service/score.type.service.ts
@@ -18,21 +18,21 @@ export class ScoreTypeService {
     async createScoreType(name: string, type: string, description: string): Promise<void> {
         const exist: ScoreType|undefined = await this.scoreTypeRepository.findOne({ name });
         if (exist) {
-            throw new HttpException("指定名称name=" + name + "积分类型已存在", 424);
+            throw new HttpException(`指定名称name=${name}积分类型已存在`, 424);
         }
         // 方法中创建的积分项都是非默认的
         const scoreType: ScoreType|undefined = this.scoreTypeRepository.create({ name, type, default: false, description });
         try {
             await this.scoreTypeRepository.save(scoreType);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async updateScoreType(id: number, name: string, type: string, description: string): Promise<void> {
         const original: ScoreType|undefined = await this.scoreTypeRepository.findOne(id);
         if (!original) {
-            throw new HttpException("指定id=" + id + "积分类型不存在", 425);
+            throw new HttpException(`指定id=${id}积分类型不存在`, 425);
         }
         if (original.default) {
             throw new HttpException("默认积分类型不允许更改", 426);
@@ -40,7 +40,7 @@ export class ScoreTypeService {
         if (name !== original.name) {
             const exist: ScoreType|undefined = await this.scoreTypeRepository.findOne({ name });
             if (exist) {
-                throw new HttpException("指定名称name=" + name + "积分类型已存在", 424);
+                throw new HttpException(`指定名称name=${name}积分类型已存在`, 424);
             }
         }
         try {
@@ -49,14 +49,14 @@ export class ScoreTypeService {
             original.description = description;
             await this.scoreTypeRepository.save(original);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
     async deleteScoreType(id: number): Promise<void> {
         const exist: ScoreType|undefined = await this.scoreTypeRepository.findOne(id);
         if (!exist) {
-            throw new HttpException("指定id=" + id + "积分类型不存在", 425);
+            throw new HttpException(`指定id=${id}积分类型不存在`, 425);
         }
         if (exist.default) {
             throw new HttpException("默认积分类型不允许删除", 426);
@@ -64,7 +64,7 @@ export class ScoreTypeService {
         try {
             await this.scoreTypeRepository.remove(exist);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 
@@ -75,7 +75,7 @@ export class ScoreTypeService {
                 return exist.id === id;
             });
             if (!find) {
-                throw new HttpException("指定id=" + id + "积分类型不存在", 425);
+                throw new HttpException(`指定id=${id}积分类型不存在`, 425);
             }
             if (find.default) {
                 throw new HttpException("默认积分类型不允许删除", 426);
@@ -84,7 +84,7 @@ export class ScoreTypeService {
         try {
             await this.scoreTypeRepository.remove(exists);
         } catch (err) {
-            throw new HttpException("数据库错误" + err.toString(), 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 401);
         }
     }
 }
